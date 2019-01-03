739ccfab7f6bc53fcd5af05f9d8a00254ebed144
Param-Harrison <paramanantham@live.com>
2019-01-03 16:24:50 +0200

    feat: initial gatsby repo cloned from one of the starter template

---
+++ config/site.js
@@ -0,0 +1,18 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  title: 'Learn with Param', // Navigation and Site Title
+  titleAlt: 'Learn with Param', // Title for JSON LD
+  description: 'Learn web and mobile development through short videos',
+  url: 'https://paramlabs.github.io', // Domain of your site. No trailing slash!
+  siteUrl: 'https://paramlabs.github.io', // url + pathPrefix
+  siteLanguage: 'en', // Language Tag on <html> element
+  logo: 'static/logo/logo.png', // Used for SEO
+  banner: 'static/logo/banner.png',
+  // JSON LD / Manifest
+  favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
+  shortName: 'param', // shortName for manifest. MUST be shorter than 12 characters
+  author: 'Param', // Author for schemaORG JSON LD
+  themeColor: '#3e7bf2',
+  backgroundColor: '#d3e0ff',
+  twitter: '@jacknapes', // Twitter Username
+};
